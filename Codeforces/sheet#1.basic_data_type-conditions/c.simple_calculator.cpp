58993b141d4cab4a90ac3d11b18ec38c1ba6f04c
mashfiqur11 <mashfiqur07@gmail.com>
2023-12-17 23:03:17 +0600

    adding simple calculator

---
+++ Codeforces/sheet#1.basic_data_type-conditions/c.simple_calculator.cpp
@@ -0,0 +1,22 @@
+/// Given two numbers X and Y. Print the summation and multiplication and subtraction of these 2 numbers.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main()
+{
+    optimize();
+
+    long long a, b;     /// check what is the a,b range.
+    cin >> a >> b;
+
+    cout << a << " " << "+" << " " << b << " " << "=" << " " << a + b << endl;
+    cout << a << " " << "*" << " " << b << " " << "=" << " " << a * b << endl;
+    cout << a << " " << "-" << " " << b << " " << "=" << " "  << a - b << endl;
+
+    return 0;
+}
