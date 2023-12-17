62c8fdf5b1f7f25d0bbf837fce71413ea150358c
mashfiqur11 <mashfiqur07@gmail.com>
2023-12-17 07:18:17 +0600

    Adding three times problem solve

---
+++ Atcoder/three_times.cpp
@@ -0,0 +1,24 @@
+/// ABC 333. problem no . A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << n;
+    }
+    
+
+    return 0;
+}
