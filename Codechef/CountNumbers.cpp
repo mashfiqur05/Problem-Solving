4aaa3531cf05a84d021558e66fbcfce9d2eb08e4
mashfiqur11 <mashfiqur07@gmail.com>
2024-04-15 21:21:40 +0600

    Adding another contest a, b, and c no problem

---
+++ Codechef/CountNumbers.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define Bismillah() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);    ///  Fast IO.
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define pb push_back
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sq(a) ((a) * (a))
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+
+//ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+//ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+//const ll MX = 123;
+//vector<ll> arr(MX);
+
+int main()
+{
+    Bismillah();
+
+    int testcases = 1;
+    cin >> testcases;
+    for (int tt = 1; tt <= testcases; tt++)
+    {
+        int cnt = 0;
+
+        vector<char> v;
+        v.clear();
+        while (1)
+        {
+            char a;
+            cin >> a;
+            if (a == '\0')
+            {
+                break;
+            }
+            v.pb (a);
+        }    
+
+        cout << v.size() << endl;
+    }
+    
+    return 0;
+}
---
+++ Codechef/football_training.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define Bismillah() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);    ///  Fast IO.
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sq(a) ((a) * (a))
+#define all(v) (v).begin(),(v).end()     /// ascending
+#define rall(v) (v).rbegin(),(v).rend()  /// descending
+
+//ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+//ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+//const ll MX = 123;
+//int arr[MX];
+
+int main()
+{
+    Bismillah();
+
+    int testcases = 1;
+    //cin >> testcases;
+    for (int tt = 1; tt <= testcases; tt++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (x > y) cout << "FREEKICK";
+        else cout << "PENALTY";
+    }
+    
+    return 0;
+}
